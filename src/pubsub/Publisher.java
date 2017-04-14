66447e201fd231b053440eac9657fe2d4da211b1
Taaha Bin Mohsin <taahabinmohsin@Taahas-MacBook-Pro.local>
2017-04-14 15:10:44 -0500

    Added Worker and Publisher skeleton files

--- src/pubsub/Publisher.java
+++ src/pubsub/Publisher.java
@@ -1,5 +1,10 @@
 package pubsub;
 
-public class Publisher {
+import pubsub.Message;
+import pubsub.Worker;
 
+public class Publisher {
+	public void publishMessage(Message msg, Worker worker){
+		
+	};
 }
