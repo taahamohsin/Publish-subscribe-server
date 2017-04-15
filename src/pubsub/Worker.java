cc7667bc5770b7ea081ab751452d896e1dff2b30
Taaha Bin Mohsin <taahabinmohsin@Taahas-MacBook-Pro.local>
2017-04-15 18:21:39 -0500

    Updated Worker class

--- src/pubsub/Worker.java
+++ src/pubsub/Worker.java
@@ -7,82 +7,80 @@ import pubsub.Message;
 import pubsub.Subscriber;
 
 public class Worker {
-	
-	HashMap<String, ArrayList<Subscriber>> subTopList=new HashMap<String, ArrayList<Subscriber>>();
-	ConcurrentLinkedQueue<Message> msgQueue=new ConcurrentLinkedQueue<Message>();
-	
-	public void insertMessage(Message msg){
+
+	// To store the topic-to-subscriber mappings
+	HashMap<String, ArrayList<Subscriber>> topSubList = new HashMap<String, ArrayList<Subscriber>>();
+	// to store the published messages
+	ConcurrentLinkedQueue<Message> msgQueue = new ConcurrentLinkedQueue<Message>();
+
+	public void insertMessage(Message msg) {
 		msgQueue.add(msg);
 	}
-	
-	public HashMap<String, ArrayList<Subscriber>> fetchSubscribers(){
-		return this.subTopList;
+
+	public HashMap<String, ArrayList<Subscriber>> fetchSubscribers() {
+		return this.topSubList;
 	}
-	
-	public void insertSubscriber(Subscriber sub, String topic){
+
+	public void insertSubscriber(Subscriber sub, String topic) {
 		ArrayList<Subscriber> tmp;
-		if(subTopList.containsKey(topic)){
-			tmp=subTopList.get(topic);
+		if (topSubList.containsKey(topic)) {
+			tmp = topSubList.get(topic);
 			tmp.add(sub);
-			subTopList.put(topic, tmp);
-		}
-		else{
-			tmp=new ArrayList<Subscriber>();
+			topSubList.put(topic, tmp);
+		} else {
+			tmp = new ArrayList<Subscriber>();
 			tmp.add(sub);
-			subTopList.put(topic,tmp);
+			topSubList.put(topic, tmp);
 		}
 	}
-	
-	public void deleteSubscriber(Subscriber sub, String topic){
-		if(subTopList.containsKey(topic)){
-			ArrayList<Subscriber> tmp=subTopList.get(topic);
+
+	public void deleteSubscriber(Subscriber sub, String topic) {
+		if (topSubList.containsKey(topic)) {
+			ArrayList<Subscriber> tmp = topSubList.get(topic);
 			tmp.remove(sub);
-			subTopList.put(topic, tmp);
-		}
-		else{
+			topSubList.put(topic, tmp);
+		} else {
 			System.out.println("There is no such subscriber for that topic!");
 		}
 	}
-	
-	public void fetchMessages(Subscriber sub, String topic){
-		boolean empty=this.msgQueue.isEmpty();
-		if(!empty){
-			while(!empty){
-				Message tmp=msgQueue.remove();
-				if(tmp.fetchTopic().equalsIgnoreCase(topic)){
-					ArrayList<Subscriber> list=subTopList.get(topic);
-					for(Subscriber sub2: list){
-						if(sub2.equals(sub)){
-							ArrayList<Message> messages=sub.fetchMessages();
+
+	public void fetchMessagesForSpecificSub(Subscriber sub, String topic) {
+		boolean empty = this.msgQueue.isEmpty();
+		if (!empty) {
+			while (!empty) {
+				Message tmp = msgQueue.remove();
+				if (tmp.fetchTopic().equalsIgnoreCase(topic)) {
+					ArrayList<Subscriber> list = topSubList.get(topic);
+					for (Subscriber sub2 : list) {
+						if (sub2.equals(sub)) {
+							ArrayList<Message> messages = sub.fetchMessages();
 							messages.add(tmp);
 							sub.setMessages(messages);
 						}
 					}
 				}
 			}
-		}
-		else{
+		} else {
 			System.out.println("The publisher did not have any messages enqueued.");
 		}
 	}
-	
-	public void broadcastMessages(){
-		boolean empty=this.msgQueue.isEmpty();
-		if(!empty){
-			while(!empty){
-				Message tmp=this.msgQueue.remove();
-				String top=tmp.fetchTopic();
-				ArrayList<Subscriber> subList=subTopList.get(top);
-				for(Subscriber sub: subList){
-					ArrayList<Message> messages=sub.fetchMessages();
+
+	public void broadcastMessages() {
+		boolean empty = this.msgQueue.isEmpty();
+		if (!empty) {
+			while (!empty) {
+				Message tmp = this.msgQueue.remove();
+				String top = tmp.fetchTopic();
+				ArrayList<Subscriber> subList = topSubList.get(top);
+				for (Subscriber sub : subList) {
+					ArrayList<Message> messages = sub.fetchMessages();
 					messages.add(tmp);
 					sub.setMessages(messages);
-				}			
+				}
 			}
-		}
-		else{
+		} else {
 			System.out.println("No received messages to display.");
 		}
 	}
-	
+
 }
