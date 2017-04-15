11a4ce21fd4e76903443b327ccae723f8feed55f
Taaha Bin Mohsin <taahabinmohsin@Taahas-MacBook-Pro.local>
2017-04-15 13:59:22 -0500

    Updated subscriber class

--- src/pubsub/Message.java
+++ src/pubsub/Message.java
@@ -1,45 +1,41 @@
 package pubsub;
 
 public class Message {
-	
+
 	private String pLoad; // payload for the message
 	private String topic; // the topic to which it pertains
-	
+
 	// Default constructor for the Message class
-	public Message(){		
+	public Message() {
 	}
-	
+
 	// Alternate constructor for the Message class
-	public Message(String pLoad, String topic){
-		this.pLoad=pLoad;
-		this.topic=topic;
+	public Message(String pLoad, String topic) {
+		this.pLoad = pLoad;
+		this.topic = topic;
 	}
-	
+
 	/* Accessor methods for the Message class */
-	
+
 	// Retrieves the value of the pLoad field
-	public String fetchPLoad(){
+	public String fetchPLoad() {
 		return this.pLoad;
 	}
-	
+
 	// Retrieves the value of the topic field
-	public String fetchTopic(){
+	public String fetchTopic() {
 		return this.topic;
 	}
 
-	/* Mutator methods for the Message class*/
-	
+	/* Mutator methods for the Message class */
+
 	// Alters the value of the payload field
-	public void setPLoad(String pLoad){
-		this.pLoad=pLoad;
+	public void setPLoad(String pLoad) {
+		this.pLoad = pLoad;
 	}
-	
+
 	// Alters the value of the topic field
-	public void setTopic(String topic){
-		this.topic=topic;
+	public void setTopic(String topic) {
+		this.topic = topic;
 	}
-	
 }
-	
-
-
--- src/pubsub/Subscriber.java
+++ src/pubsub/Subscriber.java
@@ -5,21 +5,27 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Subscriber {
-
-	private ArrayList<Message> Messages=new ArrayList<Message>();
 	
-	public ArrayList<Message> getMessages(){
+	private ArrayList<Message> Messages=new ArrayList<Message>(); // to store all received messages
+	
+	// Accessor method for received messages
+	public ArrayList<Message> fetchMessages(){
 		return Messages;
 	} 
 	
+	// Mutator method for received messages
 	public void setMessages(ArrayList<Message> Messages){
 		this.Messages=Messages;
 	}
 
+	// To print all received messages to the console
 	public void print(){
-		Iterator iter=Messages.iterator();
-		while(iter.hasNext()){
-			System.out.println("Topic: ");
+		Iterator<Message> iter=Messages.iterator();
+		while(iter.hasNext())
+		{
+		Message tmp=iter.next();
+		System.out.println("Topic: "+tmp.fetchTopic()+" and payload: "+
+		tmp.fetchPLoad());
 		}
 	}
 }
