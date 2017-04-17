dcd3af57b12af4f1127e20181f6f3c0c4ed4ab4b
Kenny Wang <test@myMacAirmatoMacBook-Air.local>
2017-04-17 16:58:17 -0500

    SimplifiedClient.java with try-with-resources

--- src/clients/Client.java
+++ src/clients/Client.java
@@ -1,12 +1,7 @@
 package clients;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
+import java.io.*;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class Client {
 
@@ -14,49 +9,19 @@ public class Client {
 		String host = "localhost";
 		int port = 8080;
 
-		// Create a socket connection to the server
-		Socket s = null;
-
-		try {
-			s = new Socket(host, port);
-
-			// Catch UnknownHostException, close the socket and exit
-		} catch (UnknownHostException e) {
-
-			System.out.println("Host not found");
-			System.exit(-1);
-
-			// Catch IOException, close the socket and exit
-		} catch (IOException e) {
-
-			System.out.println("Cannot connect");
-			System.exit(-1);
-
-		} // end outer try-catch
-
 		String name = "Kenny";
 
-		BufferedReader read = null;
-		PrintStream ps = null;
-		try {
-			ps = new PrintStream(s.getOutputStream());
-			read = new BufferedReader(new InputStreamReader(s.getInputStream()));
-			System.out.println(read.readLine());
-			System.out.println(read.readLine());
+		try (Socket s = new Socket(host, port);
+				BufferedReader read = new BufferedReader
+						(new InputStreamReader(s.getInputStream()));
+				PrintStream ps = new PrintStream(s.getOutputStream())) {
+			
+			System.out.println(read.readLine() + read.readLine());
 			ps.print(name);
 			System.out.println(name);
+			
 		} catch (IOException e) {
 			System.out.println("Error reading from the server");
-
-
-			// Make sure to that the socket is eventually closed
-			if (s != null) {
-				try {
-					s.close();
-				} catch (IOException ignored) {
-				}
-			}
-
 		}
 	}
 
